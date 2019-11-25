e6e531cec2085b003a7657f384733012bc9156a9
Fernando Cruz <ffdacruz@msn.com>
2019-11-25 13:58:37 -0300

    Started

---
+++ FlappyBird01/Assets/Scripts/Generate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generate : MonoBehaviour
+{
+    public GameObject canos;
+	int score = 0;
+	
+	// Use this for initialization
+	void Start()
+	{
+		InvokeRepeating("CreateObstacle", 1f, 1.5f);
+	}
+	
+	// Update is called once per frame
+	void OnGUI () 
+	{
+		GUI.color = Color.white;
+		GUI.skin.label.fontSize = 50;
+        GUILayout.Label(" Pontos: " + score.ToString());
+	}
+	
+	void CreateObstacle()
+	{
+		Instantiate(canos);
+		score++;
+	}
+}
---
+++ FlappyBird01/Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public AudioSource[] _audio = new AudioSource[2];
+    public Vector2 jumpForce = new Vector2(0, 300);
+	
+    void Start()
+    {
+        _audio[0].Play();
+    }
+
+	// Update is called once per frame
+	void Update ()
+	{
+		// Jump
+		if (Input.GetKeyUp("space"))
+		{
+            _audio[1].Play();
+			GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+			GetComponent<Rigidbody2D>().AddForce(jumpForce);
+		}
+		
+		// Die by being off screen
+		Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+		if (screenPosition.y > Screen.height || screenPosition.y < 0)
+		{
+			Die();
+		}
+	}
+	
+	// Die by collision
+	void OnCollisionEnter2D(Collision2D other)
+	{       
+		Die();
+	}
+	
+	void Die()
+	{
+		_audio[2].Play();
+		
+		SceneManager.LoadScene(0);
+	}
+}
